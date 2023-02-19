a4caa345df58be2b3ed48e465b511e3d4138dca3
Abramichev <endi79@mail.ru>
2023-02-20 02:27:16 +0300

    add allure report

---
+++ src/test/java/constant/Header.java
@@ -0,0 +1,9 @@
+package constant;
+
+public class Header {
+
+    public static final String CONTENT_TYPE = "Content-type";
+
+    public static final String APPLICATION_JSON = "application/json";
+
+}
